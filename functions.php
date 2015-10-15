eb0c82baf20a648177d986a0d70de0e84fbece79
Debbie Mowry <debbie@debbiemowry.com>
2015-10-15 07:38:45 -0400

    Initial commit of functions.php and style.css

---
+++ functions.php
@@ -0,0 +1,17 @@
+<?php
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+function theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+}
+
+// Custom Function to Include
+function favicon_link() {
+    echo '<link rel="shortcut icon" type="image/x-icon" href="www.here.com" />' . "\n";
+}
+add_action( 'wp_head', 'favicon_link' );
+
+
+
+
+
+?>
