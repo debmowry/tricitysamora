67f5789a0d2c81fbfee6ea7479a2331c84e83f74
Debbie Mowry <debbie@debbiemowry.com>
2015-10-15 07:52:44 -0400

    Adding stylesheets based on content types or areas.
    Adding all the individual stylesheets plus the updated master
    stylesheet which imports all of the individual stylesheets.

--- functions.php
+++ functions.php
@@ -1,4 +1,5 @@
 <?php
+
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
@@ -8,10 +9,5 @@ function theme_enqueue_styles() {
 function favicon_link() {
     echo '<link rel="shortcut icon" type="image/x-icon" href="www.here.com" />' . "\n";
 }
+
 add_action( 'wp_head', 'favicon_link' );
-
-
-
-
-
-?>
