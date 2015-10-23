598b2f9b065ab07a3fb4f3dc9d20af2b68140231
zorinsamji <zorin16@gmail.com>
2015-10-22 20:49:33 -0700

    for tricitysamora theme updated functions.php

--- functions.php
+++ functions.php
@@ -1,9 +1,10 @@
 <?php
 
-define(CALENDAR_ID,"3citysamoraseller@gmail.com");
-define(KEY, "AIzaSyBcSX2QJtx4Bf1InL0r5d4ekoMt0WJ6pAs");
+// global variables
+
 define(MAX_RESULTS,15);
 
+// functions for child theme
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
@@ -16,126 +17,91 @@ function favicon_link() {
 
 add_action( 'wp_head', 'favicon_link' );
 
+// function to display embedded google calendar
 function displayDayCalendar(){
-	$today = date("Ymd");
-	$tomorrow  = date('Ymd', strtotime('+24 hours'));
-	
-	$calendar_url = "https://www.google.com/calendar/embed?mode=DAY&amp;dates=".$today."%2F".$tomorrow."&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=3citysamoraseller%40gmail.com&amp;color=%231B887A&amp;ctz=America%2FChicago;";
-	return 	$calendar_url;
+  $today = date("Ymd");
+  $tomorrow  = date('Ymd', strtotime('+24 hours'));
+  
+  $calendar_url = "https://www.google.com/calendar/embed?mode=DAY&amp;dates=".$today."%2F".$tomorrow."&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=3citysamoraseller%40gmail.com&amp;color=%231B887A&amp;ctz=America%2FChicago;";
+  return  $calendar_url;
 
 }
 add_shortcode('displayDayCalendar', 'displayDayCalendar');
 
 
-$clientLibraryPath = '/var/www/html/ImagineArt/google-api-php-client/src';
+//SET the following variables in wp-config file
+// public or limited calendar to view ie google account
+//define('CALENDAR_ID', 'ENTERADDRESSHERE@gmail.com');
+
+//The Google API to access the google account above from this from console.developer.google.com 
+//define('GOOGLE_API', 'ENTERAPICODE_HERE');
+
+// The Google client files  stored on the webserver*/
+//define('GOOGLE_CLIENT_FILES', 'ENTER YOUR install location here/google-api-php-client/src');
+
+
+$clientLibraryPath = GOOGLE_CLIENT_FILES;
 $oldPath = set_include_path(get_include_path() . PATH_SEPARATOR . $clientLibraryPath);
-echo get_include_path();
-error_reporting(E_ALL ^ E_NOTICE);
-ini_set('display_errors', 1);
-echo "<br>";
+
+// use this when debugging
+/* echo get_include_path();
+//error_reporting(E_ALL ^ E_NOTICE);
+//ini_set('display_errors', 1);
+//echo "<br>";
+*/
+// this is a file from the following location google-api-php-client/src
 require 'Google/autoload.php';
 
-function displayListFormat_func(){
+
+function displayGCalender_func($atts){
+ $atts = shortcode_atts(
+    array(
+      'format' => 'list',
+    ), $atts, 'displayListFormat_func' );
+
+
  $client = new Google_Client();
- $client->setDeveloperKey(KEY);
+ $client->setDeveloperKey(GOOGLE_API);
+ date_default_timezone_set('America/Chicago'); 
+ $today = date(DATE_ISO8601, strtotime("now",mktime(0, 0, 0)));
+ $tomorrow  = date(DATE_ISO8601, strtotime("+1 day",mktime(0, 0, 0)));
+ $weekly  = date(DATE_ISO8601, strtotime("+7 day",mktime(0, 0, 0)));
+ $yesterday  = date(DATE_ISO8601, strtotime("-1 day",mktime(0, 0, 0)));
+ $previousStart = $yesterday;
+
   
   $gdataCal = new Google_Service_Calendar($client);
     
- 
-  $optParams = array(
+if ($atts['format']  == 'list'){
+
+    $displayHTML = "<b>List of next ".MAX_RESULTS." activities</b> <br>";
+    $optParams = array(
     'maxResults' => MAX_RESULTS,
     'orderBy' => 'startTime',
     'singleEvents' => TRUE,
     'timeMin' => date('c'),
-  );
-  $results = $gdataCal->events->listEvents(CALENDAR_ID, $optParams);
+    );
+    $results = $gdataCal->events->listEvents(CALENDAR_ID, $optParams);
+  }// end if list
 
-
-        $displayHTML = "";
-  if (count($results->getItems()) == 0) {
-            $displayHTML .= "No upcoming events found.";
-    } //end if count
-  else {
-    foreach ($results->getItems() as $event) {
-      $start = $event->start->dateTime;
-      $end = $event->end->dateTime;
-      if (empty($start)) 
-        $start = $event->start->date;
-      if (empty($end)) 
-        $end = $event->end->date;
-
-      $start = new DateTime($start);
-      $end = new DateTime($end);
-
-            $displayHTML .= "Date: ". date_format($start,'l jS F Y')."<br>";
-            $displayHTML .= date_format($start,'H:i')." to ".date_format($start,'H:i')."<br>";
-            $displayHTML .= "<a href=\"" .$event->getHtmlLink()."\">";
-            $displayHTML .= $event->getSummary(). "</a><br>";
-    }
-  }//end else echo
-
-return $displayHTML;
-}//end of function
-
-add_shortcode('displayListFormat_func', 'displayListFormat_func');
-
-function displayOneDay_func(){
- $client = new Google_Client();
- $client->setDeveloperKey(KEY);
- 
-  $today = date('c');
-  $tomorrow  = date('c', strtotime('+24 hours'));
-
-  $gdataCal = new Google_Service_Calendar($client);
-    
- 
-  $optParams = array(
-    'orderBy' => 'startTime',
-    'singleEvents' => TRUE,
-    'timeMin' => $today,
-    'timeMax' => $tomorrow,
-  );
-  $results = $gdataCal->events->listEvents(CALENDAR_ID, $optParams);
-      $displayHTML = "";
+  if ($atts['format']  == 'weekly'){
 
   
-  if (count($results->getItems()) == 0) {
-            $displayHTML .= "No upcoming events found.";
-    } //end if count
-  else {
-    foreach ($results->getItems() as $event) {
-      $start = $event->start->dateTime;
-      $end = $event->end->dateTime;
-      if (empty($start)) 
-        $start = $event->start->date;
-      if (empty($end)) 
-        $end = $event->end->date;
+  $displayHTML = "<b>Activities for the coming week</b> <br>";
 
-      $start = new DateTime($start);
-      $end = new DateTime($end);
+  $optParams = array(
+    'orderBy' => 'startTime',
+    'singleEvents' => TRUE,
+    'timeMin' => $today,
+    'timeMax' => $weekly,
+  );
+  $results = $gdataCal->events->listEvents(CALENDAR_ID, $optParams);
+  }// end if weekly
 
-            $displayHTML .= "Date: ". date_format($start,'l jS F Y')."<br>";
-            $displayHTML .= date_format($start,'H:i')." to ".date_format($start,'H:i')."<br>";
-            $displayHTML .= "<a href=\"" .$event->getHtmlLink()."\">";
-            $displayHTML .= $event->getSummary(). "</a><br>";
-    }
-  }//end else echo
-return $displayHTML;
-}//end of function
+if ($atts['format']  == 'day'){
 
 
-add_shortcode('displayOneDay_func', 'displayOneDay_func');
-
-function displayOneWeek_func(){
-$client = new Google_Client();
- $client->setDeveloperKey(KEY);
- 
-  $today = date('c');
-  $tomorrow  = date('c', strtotime('+7 days'));
-
-  $gdataCal = new Google_Service_Calendar($client);
-    
- 
+ $displayHTML = "<b>Today's Activities </b></br>";
   $optParams = array(
     'orderBy' => 'startTime',
     'singleEvents' => TRUE,
@@ -143,36 +109,46 @@ $client = new Google_Client();
     'timeMax' => $tomorrow,
   );
   $results = $gdataCal->events->listEvents(CALENDAR_ID, $optParams);
+}//end if day
 
+    if (count($results->getItems()) == 0) {
+              $displayHTML .= "No upcoming events found.";
+      } //end if count
+    else {
+      foreach ($results->getItems() as $event) {
+        $start = $event->start->dateTime;
+        $end = $event->end->dateTime;
+        if (empty($start)) 
+          $start = $event->start->date;
+        if (empty($end)) 
+          $end = $event->end->date;
 
-  $displayHTML = "";
+        $start = new DateTime($start);
+        $end = new DateTime($end);
 
-  if (count($results->getItems()) == 0) {
-      $displayHTML .= "No upcoming events found.";
-    } //end if count
-  else {
-    foreach ($results->getItems() as $event) {
-      $start = $event->start->dateTime;
-      $end = $event->end->dateTime;
-      if (empty($start)) 
-        $start = $event->start->date;
-      if (empty($end)) 
-        $end = $event->end->date;
+        $firstDate = date_format($previousStart, 'Ymd'); 
+        $secondDate = date_format($start, 'Ymd');
 
-      $start = new DateTime($start);
-      $end = new DateTime($end);
+          if ($firstDate == $secondDate){
+            $displayHTML .= date_format($start,'H:i')." to ".date_format($start,'H:i');
+            $displayHTML .= "<a href=\"" .$event->getHtmlLink()."\">";
+            $displayHTML .= $event->getSummary(). "</a><br>";
 
-      $displayHTML .= "Date: ". date_format($start,'l jS F Y')."<br>";
-      $displayHTML .= date_format($start,'H:i')." to ".date_format($start,'H:i')."<br>";
-      $displayHTML .= "<a href=\"" .$event->getHtmlLink()."\">";
-      $displayHTML .= $event->getSummary(). "</a><br>";
-    }
-  }//end else echo
+         }
+         else {
+            $displayHTML .= "". date_format($start,'l F jS Y')."<br> ";
+            $displayHTML .= date_format($start,'H:i')." to ".date_format($start,'H:i');
+            $displayHTML .= "  <a href=\"" .$event->getHtmlLink()."\">";
+            $displayHTML .= $event->getSummary(). "</a><br>";
+
+            $previousStart = $start;
+          }
+              
+      }//end for each
+    }//end else 
+
 return $displayHTML;
 }//end of function
 
 
-
-
-add_shortcode('displayOneWeek_func', 'displayOneWeek_func');
-
+add_shortcode('displayGCalender_func', 'displayGCalender_func');
